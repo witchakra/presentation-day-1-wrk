705829d9dd145fed895f49127dfbdb01236f553e
Ramida_Soisrikham <6633205621@student.chula.ac.th>
2024-03-02 20:59:10 +0700

    Add tel. in models/User.js

--- models/User.js
+++ models/User.js
@@ -7,6 +7,10 @@ const UserSchema = new mongoose.Schema({
 		type: String,
 		required: [true, 'Please add a name'],
 	},
+	tel: {
+		type: String,
+		required: [true, 'Please add a tel.'],
+	},
 	email: {
 		type: String,
 		required: [true, 'Please add an email'],
