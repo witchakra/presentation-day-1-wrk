9b0719e9dbbb4c60587a641a9b99b72efa205e52
Kanchai Triyavanich <kanhrk.08541@gmail.com>
2024-03-03 12:46:21 +0700

    edit function in controller/CoWorks

---
+++ controllers/coWorks.js
@@ -0,0 +1,156 @@
+const CoWork = require('../models/CoWork');
+const CoBranch = require('../models/CoBranch');
+
+// @desc    Get Coworking Space Branchs
+// @route   GET /api/v1/coworks/coBranchs
+// @access  Public
+exports.getCoBranchs = (req, res, next) => {
+	CoBranch.getAll((err, data) => {
+		if (err)
+			res.status(500).send({
+				message:
+					err.message ||
+					'Some error occurred while retrieving Coworking Space Branchs.',
+			});
+		else res.send(data);
+	});
+};
+
+// @desc    Get all coworks
+// @route   GET /api/v1/coworks
+// @access  Public
+exports.getCoWorks = async (req, res, next) => {
+	let query;
+
+	// Copy req.query
+	const reqQuery = {...req.query};
+
+	// Fields to exclude
+	const removeFields = ['select', 'sort', 'page', 'limit'];
+
+	// Loop over remove fields and delete them from reqQuery
+	removeFields.forEach((param) => delete reqQuery[param]);
+	console.log(reqQuery);
+
+	//Create query string
+	let queryStr = JSON.stringify(reqQuery);
+	queryStr = queryStr.replace(
+		/\b(gt|gte|lt|lte\in)\b/g,
+		(match) => `$${match}`
+	);
+	// Finding resource
+	query = CoWork.find(JSON.parse(queryStr)).populate('appointments');
+
+	// Select Fields
+	if (req.query.select) {
+		const fields = req.query.select.split(',').join(' ');
+		query = query.select(fields);
+	}
+	// Sort
+	if (req.query.sort) {
+		const sortBy = req.query.sort.split(',').join(' ');
+		query = query.sort(sortBy);
+	} else {
+		query = query.sort('name');
+	}
+	// Pagination
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 25;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+
+	try {
+		const total = await CoWork.countDocuments();
+
+		query = query.skip(startIndex).limit(limit);
+
+		// Executing query
+		const coworks = await query;
+
+		// Pagination result
+		const pagination = {};
+
+		if (endIndex < total) {
+			pagination.next = {
+				page: page + 1,
+				limit,
+			};
+		}
+
+		if (startIndex > 0) {
+			pagination.prev = {
+				page: page - 1,
+				limit,
+			};
+		}
+		res.status(200).json({
+			success: true,
+			count: coworks.length,
+			pagination,
+			data: coworks,
+		});
+	} catch (err) {
+		res.status(400).json({success: false});
+	}
+};
+
+// @desc    Get single cowork
+// @route   GET /api/v1/coworks/:id
+// @access  Public
+exports.getCoWork = async (req, res, next) => {
+	try {
+		const cowork = await CoWork.findById(req.params.id);
+
+		if (!cowork) {
+			return res.status(400).json({success: false});
+		}
+		res.status(200).json({success: true, data: cowork});
+	} catch (err) {
+		res.status(400).json({success: false});
+	}
+};
+
+// @desc    Create new coworks
+// @route   POST /api/v1/coworks
+// @access  Private
+exports.createCoWork = async (req, res, next) => {
+	const cowork = await CoWork.create(req.body);
+	res.status(201).json({success: true, data: cowork});
+};
+
+// @desc    Update Single cowork
+// @route   PUT /api/v1/coworks/:id
+// @access  Private
+exports.updateCoWork = async (req, res, next) => {
+	try {
+		const cowork = await CoWork.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!cowork) {
+			return res.status(400).json({success: false});
+		}
+		res.status(200).json({success: true, data: cowork});
+	} catch (err) {
+		res.status(400).json({success: false});
+	}
+};
+
+// @desc    Delete cowork
+// @route   DELETE /api/v1/coworks/:id
+// @access  Private
+exports.deleteCoWork = async (req, res, next) => {
+	try {
+		const cowork = await CoWork.findById(req.params.id);
+
+		if (!cowork) {
+			return res.status(404).json({success: false});
+		}
+
+		await cowork.deleteOne();
+		res.status(200).json({success: true, data: {}});
+	} catch (err) {
+		res.status(400).json({success: false});
+	}
+};
