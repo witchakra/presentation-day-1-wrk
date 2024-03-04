f8870c38a7a41bf1e5bdd4fdf4f7251e93953c62
Ramida_Soisrikham <6633205621@student.chula.ac.th>
2024-03-04 12:55:14 +0700

    Add tel in controllers/auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,11 +6,12 @@ const User = require('../models/User');
 
 exports.register = async (req, res, next) => {
 	try {
-		const {name, email, password, role} = req.body;
+		const {name, tel, email, password, role} = req.body;
 
 		//Create User
 		const user = await User.create({
 			name,
+			tel,
 			email,
 			password,
 			role,
